cf26f18ce16c0b8440d07c1e8443bbea17d56e95
Katarzyna76026 <katarzyna.warda@edu.uekat.pl>
2023-08-01 16:31:04 +0200

    screen

--- app/src/main/java/com/example/fiszki/MainActivity.kt
+++ app/src/main/java/com/example/fiszki/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.fiszki
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.fiszki.ui.theme.FiszkiTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +31,48 @@ class MainActivity : ComponentActivity() {
                 ) {
                     // a tutaj wywołujesz już metodę do aplikacji
                     //dlatego jak klikniesz run to będzie Kasia a nie Róża
-                    Greeting("Kasia")
+                    newScreen()
                 }
             }
         }
     }
 }
 
+// przejscie z ekranu 1 na 2, number to ilość przycisków z ktorych każdy będzie repersentować folder
+// a ilość folderów myśle że najprościej zrobić w liście gdzie będą przechowywane nazwy
+@Composable
+fun newScreen (){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "startingScreen") {
+        composable("startingScreen"){
+            StartingScreen(number = 5,navController)
+        }
+        composable("addingFile"){
+            AddingFile()
+        }
+    }
+}
+
 // tutaj dodajesz elementy do layout typu Text button itd itd
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StartingScreen(number: Int, navController: NavController) {
+    val files = IntArray(number)
+
+    Column () {
+        Text(text = "Miło Cię znów widzieć!")
+
+        for (file in files){
+            Button(onClick = { navController.navigate("addingFile")}) {
+                Text(text = file.toString())
+            }
+        }
+    }
+
+}
+
+@Composable
+fun AddingFile() {
+    Text(text = "Hello Android!")
 }
 
 // tutaj jest testowanie po prawej stronie się to zmiena, masz cały czas podgląd na to co się dzieje
@@ -42,6 +80,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     FiszkiTheme {
-        Greeting("Róża")
+        newScreen()
     }
 }
